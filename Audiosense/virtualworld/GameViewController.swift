efadef45b2754b5661a8869c9869408200ce93e5
Karim Abedrabbo <karimnabedrabbo@gmail.com>
2019-11-23 12:08:12 -0800

    Swift 3D Scene kit to screen hearing

--- Audiosense/virtualworld/GameViewController.swift
+++ Audiosense/virtualworld/GameViewController.swift
@@ -40,11 +40,11 @@ class GameViewController: UIViewController, SCNSceneRendererDelegate {
         super.viewDidLoad()
             }
     
-    override func viewWillAppear(animated: Bool) {
+    override func viewWillAppear(_ animated: Bool) {
         
     }
     
-    override func viewDidAppear(animated: Bool) {
+    override func viewDidAppear(_ animated: Bool) {
         for _ in (0...18){
             audioCubes.append(SCNBox())
             audioCubesNodes.append(SCNNode())
@@ -60,17 +60,17 @@ class GameViewController: UIViewController, SCNSceneRendererDelegate {
         sceneSetup()
         
         let alertController = UIAlertController(title: "Sound Localization Test", message:
-            "Each sound will play 5 times. Tilt the device to find the playing sound and tap on the block that you believe is playing the sound. The test will start after the instructions end.", preferredStyle: UIAlertControllerStyle.Alert)
+            "Each sound will play 5 times. Tilt the device to find the playing sound and tap on the block that you believe is playing the sound. The test will start after the instructions end.", preferredStyle: UIAlertControllerStyle.alert)
         
-        self.presentViewController(alertController, animated: true, completion: nil)
-        
-        
+        self.present(alertController, animated: true, completion: nil)
         
         
-        let audioPath = NSBundle.mainBundle().pathForResource("art.scnassets/IntroFinal", ofType: "wav")
+        
+        
+        let audioPath = Bundle.main.path(forResource: "art.scnassets/IntroFinal", ofType: "wav")
         
         do {
-            player = try AVAudioPlayer(contentsOfURL: NSURL(fileURLWithPath: audioPath!))
+            player = try AVAudioPlayer(contentsOf: URL(fileURLWithPath: audioPath!))
             player.volume = 10
         }
         catch {
@@ -80,9 +80,9 @@ class GameViewController: UIViewController, SCNSceneRendererDelegate {
         player.play()
         
         let delay = 15.3 * Double(NSEC_PER_SEC)
-        let time = dispatch_time(DISPATCH_TIME_NOW, Int64(delay))
-        dispatch_after(time, dispatch_get_main_queue()) {
-            alertController.addAction(UIAlertAction(title: "Start", style: UIAlertActionStyle.Default, handler: {action in self.playSounds()}))
+        let time = DispatchTime.now() + Double(Int64(delay)) / Double(NSEC_PER_SEC)
+        DispatchQueue.main.asyncAfter(deadline: time) {
+            alertController.addAction(UIAlertAction(title: "Start", style: UIAlertActionStyle.default, handler: {action in self.playSounds()}))
 
            
         }
@@ -100,7 +100,7 @@ class GameViewController: UIViewController, SCNSceneRendererDelegate {
         let scene = SCNScene()
         sceneView.scene = scene
         sceneView.sizeToFit()
-        scene.background.contents = UIColor.blackColor()
+        scene.background.contents = UIColor.black
         
         
         
@@ -112,8 +112,8 @@ class GameViewController: UIViewController, SCNSceneRendererDelegate {
         groundNode = SCNNode(geometry: ground)
         groundNode.position = SCNVector3(0,-20,0)
         groundNode.rotation = SCNQuaternion(x: 1, y: 0, z: 0, w: -1.5708)
-        ground.firstMaterial?.diffuse.contents = UIColor.blackColor()
-        ground.firstMaterial?.specular.contents = UIColor.whiteColor()
+        ground.firstMaterial?.diffuse.contents = UIColor.black
+        ground.firstMaterial?.specular.contents = UIColor.white
         ground.firstMaterial?.shininess = 20.0
         ground.firstMaterial?.reflective.contents = scene.background.contents
         scene.rootNode.addChildNode(groundNode)
@@ -136,15 +136,15 @@ class GameViewController: UIViewController, SCNSceneRendererDelegate {
         
         let lightNode = SCNNode()
         lightNode.light = SCNLight()
-        lightNode.light!.type = SCNLightTypeOmni
+        lightNode.light!.type = SCNLight.LightType.omni
         lightNode.position = SCNVector3(x: 0, y: 10, z: 10)
         scene.rootNode.addChildNode(lightNode)
         
         // create and add an ambient light to the scene
         let ambientLightNode = SCNNode()
         ambientLightNode.light = SCNLight()
-        ambientLightNode.light!.type = SCNLightTypeAmbient
-        ambientLightNode.light!.color = UIColor.darkGrayColor()
+        ambientLightNode.light!.type = SCNLight.LightType.ambient
+        ambientLightNode.light!.color = UIColor.darkGray
         scene.rootNode.addChildNode(ambientLightNode)
         
         sceneView.allowsCameraControl = true
@@ -155,18 +155,20 @@ class GameViewController: UIViewController, SCNSceneRendererDelegate {
         sceneView.addGestureRecognizer(tapGesture)
         
         
+        
         //motion manager setup
         motionManager = CMMotionManager()
         motionManager?.deviceMotionUpdateInterval = 1.0 / 60.0
-        motionManager?.startDeviceMotionUpdatesUsingReferenceFrame(CMAttitudeReferenceFrame.XArbitraryZVertical, toQueue: NSOperationQueue.mainQueue(), withHandler: { (motion : CMDeviceMotion?, error: NSError?) -> Void in
-            guard let motion = motion else {return}
-            let roll = CGFloat(motion.attitude.roll)
-            let rotateCamera = SCNAction.rotateByX(0, y: (roll * -1)/40 , z: 0.0, duration: 0.1)
-            self.cameraNode.runAction(rotateCamera)
+        let queue = OperationQueue()
+        motionManager?.startDeviceMotionUpdates(using: CMAttitudeReferenceFrame.xArbitraryZVertical, to: queue) { [weak self] (data: CMDeviceMotion?, error: Error?) in
+            guard let data = data else {return}
+            let roll = CGFloat(data.attitude.roll)
+            let rotateCamera = SCNAction.rotateBy(x: 0, y: (roll * -1)/40 , z: 0.0, duration: 0.1)
+            self?.cameraNode.runAction(rotateCamera)
             
             
             
-        })
+        }
  
     }
     
@@ -200,14 +202,14 @@ class GameViewController: UIViewController, SCNSceneRendererDelegate {
         
     }
     
-    func createSoundTowers(scene:SCNScene){
+    func createSoundTowers(_ scene:SCNScene){
         for x in (0...18) {
             audioCubes[x] = SCNBox(width: 5, height: 5, length: 5, chamferRadius: 0)
             audioCubesNodes[x] = SCNNode(geometry: audioCubes[x])
             audioCubesNodes[x].position = SCNVector3(100 * cos(Double(x) * 0.17453292519),0, 100 * -sin(Double(x) * 0.17453292519))
             scene.rootNode.addChildNode(audioCubesNodes[x])
-            audioCubes[x].firstMaterial?.diffuse.contents = UIColor.whiteColor()
-            audioCubes[x].firstMaterial?.specular.contents = UIColor.whiteColor()
+            audioCubes[x].firstMaterial?.diffuse.contents = UIColor.white
+            audioCubes[x].firstMaterial?.specular.contents = UIColor.white
             audioCubes[x].firstMaterial?.shininess = 90.0
             
             audioCubesNodes[x].castsShadow = true
@@ -222,35 +224,35 @@ class GameViewController: UIViewController, SCNSceneRendererDelegate {
         {
             let numDecider = zeroToEighteen[Int(arc4random_uniform(UInt32(zeroToEighteen.count)))]
             //  print("num: \(num)")
-            zeroToEighteen.removeAtIndex(zeroToEighteen.indexOf(numDecider)!)
+            zeroToEighteen.remove(at: zeroToEighteen.index(of: numDecider)!)
             //    print("oneToEighteen: \(oneToEighteen)")
             
             audioCubesNodes[x].addChildNode(audioNodes[x])
-            audioSources[x].positional = true
+            audioSources[x].isPositional = true
             audioSources[x].loops = false
             audioSources[x].volume = 2
             
             switch numDecider {
             case 0,1  :
-                audioSources[x] = SCNAudioSource(named: "art.scnassets/NEW 250.wav")!
+                audioSources[x] = SCNAudioSource(named: "art.scnassets/250.wav")!
             case 2,3  :
-                audioSources[x] = SCNAudioSource(named: "art.scnassets/NEW 500.wav")!
+                audioSources[x] = SCNAudioSource(named: "art.scnassets/500.wav")!
             case 4,5  :
-                audioSources[x] = SCNAudioSource(named: "art.scnassets/NEW 750.wav")!
+                audioSources[x] = SCNAudioSource(named: "art.scnassets/750.wav")!
             case 6,7  :
-                audioSources[x] = SCNAudioSource(named: "art.scnassets/NEW 1000.wav")!
+                audioSources[x] = SCNAudioSource(named: "art.scnassets/1000.wav")!
             case 8,9  :
-                audioSources[x] = SCNAudioSource(named: "art.scnassets/NEW 2000.wav")!
+                audioSources[x] = SCNAudioSource(named: "art.scnassets/2000.wav")!
             case 10,11  :
-                audioSources[x] = SCNAudioSource(named: "art.scnassets/NEW 3000.wav")!
+                audioSources[x] = SCNAudioSource(named: "art.scnassets/3000.wav")!
             case 12,13  :
-                audioSources[x] = SCNAudioSource(named: "art.scnassets/NEW 4000.wav")!
+                audioSources[x] = SCNAudioSource(named: "art.scnassets/4000.wav")!
             case 14,15  :
-                audioSources[x] = SCNAudioSource(named: "art.scnassets/NEW 6000.wav")!
+                audioSources[x] = SCNAudioSource(named: "art.scnassets/6000.wav")!
             case 16,17  :
-                audioSources[x] = SCNAudioSource(named: "art.scnassets/NEW 8000.wav")!
+                audioSources[x] = SCNAudioSource(named: "art.scnassets/8000.wav")!
             default :
-                let frequency = ["NEW 250","NEW 500","NEW 750","NEW 1000","NEW 2000","NEW 3000","NEW 4000","NEW 6000","NEW 8000"]
+                let frequency = ["250","500","750","1000","2000","3000","4000","6000","8000"]
                 let frequencyDecider = frequency[Int(arc4random_uniform(UInt32(frequency.count)))]
                 SCNAudioSource(named: "art.scnassets/\(frequencyDecider).wav")!
             }
@@ -258,13 +260,13 @@ class GameViewController: UIViewController, SCNSceneRendererDelegate {
         }
     }
     
-    func handleTap(gestureRecognize: UIGestureRecognizer) {
+    func handleTap(_ gestureRecognize: UIGestureRecognizer) {
         // retrieve the SCNView
         let scnView = self.view as! SCNView
         
         
         // check what nodes are tapped
-        let tap = gestureRecognize.locationInView(scnView)
+        let tap = gestureRecognize.location(in: scnView)
         let hitResults = scnView.hitTest(tap, options: nil)
         // check that we clicked on at least one object
         if hitResults.count > 0 {
@@ -289,26 +291,17 @@ class GameViewController: UIViewController, SCNSceneRendererDelegate {
             
             
             let delay = 0.75 * Double(NSEC_PER_SEC)
-            let time = dispatch_time(DISPATCH_TIME_NOW, Int64(delay))
-            dispatch_after(time, dispatch_get_main_queue()) {
+            let time = DispatchTime.now() + Double(Int64(delay)) / Double(NSEC_PER_SEC)
+            DispatchQueue.main.asyncAfter(deadline: time) {
                 self.playSounds()
             }
             
             userTapsOnCubes += 1
     
+            
             // highlight it
             SCNTransaction.begin()
-            SCNTransaction.setAnimationDuration(0.1)
-            
-            // on completion - unhighlight
-            SCNTransaction.setCompletionBlock {
-                SCNTransaction.begin()
-                SCNTransaction.setAnimationDuration(0.1)
-                
-                material.diffuse.contents = UIColor.whiteColor()
-                SCNTransaction.commit()
-            }
-            
+            SCNTransaction.animationDuration = 0.1
             material.diffuse.contents = UIColor(red: 0, green: 0, blue: 75, alpha: 1)
             SCNTransaction.commit()
             
@@ -324,10 +317,10 @@ class GameViewController: UIViewController, SCNSceneRendererDelegate {
     if(userTapsOnCubes == 9)
     {
     let alertController = UIAlertController(title: "Results", message:
-    "You got \(totalCorrect)/9 correct. Tap on a block to restart.", preferredStyle: UIAlertControllerStyle.Alert)
-        alertController.addAction(UIAlertAction(title: "Restart", style: UIAlertActionStyle.Default, handler: {action in self.refreshView()}))
+    "You got \(totalCorrect)/9 correct. Tap on a block to restart.", preferredStyle: UIAlertControllerStyle.alert)
+        alertController.addAction(UIAlertAction(title: "Restart", style: UIAlertActionStyle.default, handler: {action in self.refreshView()}))
 
-    self.presentViewController(alertController, animated: true, completion: nil)
+    self.present(alertController, animated: true, completion: nil)
         
         
     }
@@ -340,7 +333,7 @@ class GameViewController: UIViewController, SCNSceneRendererDelegate {
     print("block tapped: \(blockTapped)")
         print("block playing: \(numDeciderInPlaySounds)")
         print()
-        zeroToEighteenInPlaySounds.removeAtIndex(zeroToEighteenInPlaySounds.indexOf(numDeciderInPlaySounds)!)
+        zeroToEighteenInPlaySounds.remove(at: zeroToEighteenInPlaySounds.index(of: numDeciderInPlaySounds)!)
     audioPlayers[numDeciderInPlaySounds] = SCNAudioPlayer(source: audioSources[numDeciderInPlaySounds])
     audioNodes[numDeciderInPlaySounds].addAudioPlayer(audioPlayers[numDeciderInPlaySounds]!)
         
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Audiosense
 Audiosense is an IOS mobile application built to diagnose hearing and utilizes 3-D sound localization.
+
+![Alt text](./audiosense_poster.png?raw=true "Audiosense Poster")
